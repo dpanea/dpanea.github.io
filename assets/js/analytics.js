be78cb491e444744df2eb360f2e8b48b4f3f8eb5
dpanea <dan.panea@gmail.com>
2026-05-05 08:48:15 +0200

    disable analytics for myself

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,8 +1,20 @@
 (() => {
+    const optOutKey = 'dpAnalyticsOptOut';
+    const params = new URLSearchParams(window.location.search);
+
+    if (params.get('analytics') === 'off') {
+        localStorage.setItem(optOutKey, 'true');
+    }
+
+    if (params.get('analytics') === 'on') {
+        localStorage.removeItem(optOutKey);
+    }
+
+    const analyticsDisabled = localStorage.getItem(optOutKey) === 'true';
     const token = window.CF_WEB_ANALYTICS_TOKEN || 'a91debbd288543ad9e4e947f3ba2bcb5';
     const hasToken = /^[a-f0-9]{32}$/i.test(token);
 
-    if (hasToken) {
+    if (!analyticsDisabled && hasToken) {
         const script = document.createElement('script');
         script.defer = true;
         script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
